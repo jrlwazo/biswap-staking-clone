bf2850563835ad6ba47cdc7dbb7ec7301c03b431
jrl <89223219+jrlwazo@users.noreply.github.com>
2022-01-07 20:34:20 -0500

    + more test

--- test/GXOToken.test.js
+++ test/GXOToken.test.js
@@ -10,28 +10,45 @@ const GeometryToken = artifacts.require('GXOToken');
 contract('GXOToken', ([alice, bob, carol, dev, refFeeAddr, safuAddr, minter]) => {
     beforeEach(async () => {
         //deploy 
-        this.geometry = await GeometryToken.new({ from: minter });
-        console.log((await this.geometry.name()));
-
+        this.gxo = await GeometryToken.new({ from: minter });
 
         // add minter
-        await this.geometry.addMinter(minter, { from: minter });
-        // await this.geometry.addMinter(this.chef.address, { from: minter });
-        await this.geometry.mint(alice, "1", { from: minter });
-        //await this.geometry.transferOwnership(this.chef.address, { from: minter });
+        await this.gxo.addMinter(minter, { from: minter });
+        // await this.gxo.addMinter(this.chef.address, { from: minter });
+        await this.gxo.mint(alice, "10", { from: minter });
+        //await this.gxo.transferOwnership(this.chef.address, { from: minter });
 
 
 
     });
 
     it('should have a name and symbol', async () => {
-        expect((await this.geometry.name()).toString()).to.be.equal("Geometry");
-        assert.equal(await this.geometry.symbol(), 'GXO');
+        expect((await this.gxo.name()).toString()).to.be.equal("Geometry");
+        assert.equal(await this.gxo.symbol(), 'GXO');
     });
     it('should have a minter with value address', async () => {
-        expect((await this.geometry.getMinter(0, { from: minter }))).to.be.properAddress;
+        expect((await this.gxo.getMinter(0, { from: minter }))).to.be.properAddress;
 
     });
+    it('alice should have 10 GXO tokens', async () => {
+        expect((await this.gxo.balanceOf(alice)).toString()).to.equal("10");
+
+    });
+
+    it('alice should delegate 5 GXO tokens', async () => {
+        console.log("alice addr", alice);
+        console.log(await this.gxo.delegates(alice));
+
+        await this.gxo.delegate(bob, { from: alice });
+        //    expect(await this.delegates(bob))
+
+        console.log("alice addr", alice);
+        console.log(await this.gxo.delegates(alice));
+        console.log("bob addr", bob);
+        console.log(await this.gxo.delegates(bob));
+
+    });
+
 
 
 
