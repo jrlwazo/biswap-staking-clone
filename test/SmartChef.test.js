f1601c9af4d37e74d6c366ccd61b8a482cdd2966
jrl <89223219+jrlwazo@users.noreply.github.com>
2022-01-13 19:16:19 -0500

    + Mock ERC20; MOD: smartchef test

--- test/SmartChef.test.js
+++ test/SmartChef.test.js
@@ -3,7 +3,7 @@ const { assert } = require('chai');
 const JSBI = require('jsbi')
 const AURAToken = artifacts.require('AURAToken');
 const SmartChef = artifacts.require('SmartChef');
-const MockBEP20 = artifacts.require('libs/MockBEP20');
+const MockBEP20 = artifacts.require('MockERC20');
 const perBlock = '100';
 contract('SmartChef', ([alice, bob, carol, dev, refFeeAddr, minter]) => {
     beforeEach(async () => {
@@ -19,42 +19,44 @@ contract('SmartChef', ([alice, bob, carol, dev, refFeeAddr, minter]) => {
         await this.rewardToken.transfer(this.chef.address, '1000', { from: minter });
         console.log('balance chef for reward token: ', (await this.rewardToken.balanceOf(this.chef.address)).toString());
     });
-    it('real case', async () => {
+    describe("basic scenario", async () => {
+        beforeEach("Approve and stake Aura; Wait for time to pass.", async () => {
+            console.log((await time.latestBlock()).toString());
+            await time.advanceBlockTo("200");
+            // approve address
+            await this.aura.approve(this.chef.address, '1000', { from: alice });
+            await this.aura.approve(this.chef.address, '1000', { from: bob });
+            // deposit LP in chef
+            await this.chef.deposit('1', { from: alice });
+            await this.chef.deposit('1', { from: bob });
 
-        console.log((await time.latestBlock()).toString());
-        // approve address
-        await this.aura.approve(this.chef.address, '1000', { from: alice });
-        await this.aura.approve(this.chef.address, '1000', { from: bob });
-        // deposit LP in chef
-        await this.chef.deposit('1', { from: alice });
-        await this.chef.deposit('1', { from: bob });
+            assert.equal((await this.aura.balanceOf(alice)).toString(), '999');
+            assert.equal((await this.aura.balanceOf(bob)).toString(), '999');
+            assert.equal((await this.aura.balanceOf(this.chef.address)).toString(), '2');
+            await time.advanceBlockTo('210');
+        });
 
-        let aliseAURABalance = await this.aura.balanceOf(alice);
-        console.log('alise aura balance: ', aliseAURABalance.toString());
-        console.log('balance aura for chef: ', (await this.aura.balanceOf(this.chef.address)).toString());
-        let bobAURABalance = await this.aura.balanceOf(bob);
-        console.log('bob aura balance: ', bobAURABalance.toString());
-        console.log('balance aura for chef: ', (await this.aura.balanceOf(this.chef.address)).toString());
+        it('should withdraw AURA and receive reward token', async () => {
 
-        // wait a few blocks before withdrawing.
-        // var latestBlock = (await time.latestBlock()).toString();
-        // console.log(latestBlock);
-        await time.advanceBlockTo('225');
+            await this.chef.withdraw('1', { from: alice });
 
-        await this.chef.withdraw('1', { from: alice });
-        await this.chef.withdraw('1', { from: bob });
-        // await this.chef.withdrawRefFee({ from: minter });
-        console.log('-----');
-        aliseAURABalance = await this.aura.balanceOf(alice);
-        bobAURABalance = await this.aura.balanceOf(bob);
-        console.log('alise aura balance: ', aliseAURABalance.toString());
-        console.log('balance aura for chef: ', (await this.aura.balanceOf(this.chef.address)).toString());
-        console.log('balance reward token for alice: ', (await this.rewardToken.balanceOf(alice)).toString());
-        console.log('bob aura balance: ', bobAURABalance.toString());
-        console.log('balance aura for chef: ', (await this.aura.balanceOf(this.chef.address)).toString());
-        console.log('balance reward token for bob: ', (await this.rewardToken.balanceOf(bob)).toString());
-        console.log('balance reward token for ref fee: ', (await this.rewardToken.balanceOf(refFeeAddr)).toString());
+            await this.chef.withdraw('1', { from: bob });
 
-    })
+            // await this.chef.withdrawRefFee({ from: minter });
+            console.log('-----');
+
+            assert.equal((await this.aura.balanceOf(alice)).toString(), '1000');
+            assert.equal((await this.aura.balanceOf(bob)).toString(), '1000');
+            assert.equal((await this.aura.balanceOf(this.chef.address)).toString(), '0');
+            assert.equal((await this.rewardToken.balanceOf(alice)).toString(), '450');
+            assert.equal((await this.rewardToken.balanceOf(bob)).toString(), '450');
+            assert.equal((await this.rewardToken.balanceOf(this.chef.address)).toString(), '100');
+
+            // console.log('balance reward token for ref fee: ', (await this.rewardToken.balanceOf(refFeeAddr)).toString());
+
+        })
+
+    });
+
 
 });
